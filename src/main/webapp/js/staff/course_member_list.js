78f5a81dafbecfb2b5f612cf70e1990efa3314e3
Natalie <e810407@gmail.com>
2022-10-22 11:37:06 +0800

    新增團課查看已報名清單功能

--- src/main/webapp/js/staff/course_member_list.js
+++ src/main/webapp/js/staff/course_member_list.js
@@ -6,8 +6,8 @@
     { gender: 1, value: "女" }
   ];
 
-   // getById拿該堂課會員
-  const courseId = sessionStorage.getItem('course');
+   // getById拿有預約該團課的會員
+  const courseId = parseInt(sessionStorage.getItem('course'));
 	fetch("http://localhost:8080/simplefitness-servlet/course/GetCourseMemberList", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -17,19 +17,33 @@
   })
 	.then(resp => resp.json())
 	.then(list => {
-		for (i = 0; i < list["length"]; i++){
-      let bookTime = moment(list[i].coursebookTime).format("YYYY-MM-DD HH:mm");
-		  let text = `
-		  	<tr>
-				<td>${list[i].memName}</td>
-				<td>${gender[list[i].memGender].value}</td>
-				<td>${list[i].memPhone}</td>
-				<td>${bookTime}</td>
-	  		</tr>`;
-
-			$(".info").append(text);
-		}
-	
+		successful = false;
+    if (list != "" ) {
+      successful = list[0].successful;
+    }
+    if (successful) {
+      for (i = 0; i < list["length"]; i++){
+        let gym = sessionStorage.getItem(`'gym${list[i].gymId}'`);
+        let emp = sessionStorage.getItem(`'emp${list[i].empId}'`);
+        let bookTime = moment(list[i].coursebookTime).format("YYYY-MM-DD HH:mm");
+        let startTime = moment(list[i].startTime).format("YYYY-MM-DD HH:mm");
+        let text = `
+          <tr>
+            <td>${list[i].memName}</td>
+            <td>${gender[list[i].memGender].value}</td>
+            <td>${list[i].memPhone}</td>
+            <td>${bookTime}</td>
+          </tr>`;
+        document.querySelector('#gym').textContent = gym;
+        document.querySelector('#startTime').textContent = startTime;
+        document.querySelector('#name').textContent = list[i].courseName;
+        document.querySelector('#emp').textContent = emp;
+        $(".info").append(text);
+      }
+    } else {
+      let text = `<div>目前還沒有人預約!</div>`;
+      $(".info").append(text);
+    }
 	});
 
 })();

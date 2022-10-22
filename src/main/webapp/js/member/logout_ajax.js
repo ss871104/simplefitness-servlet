c89d487c32bc87ff8f21552175cd06cadad4f453
Iris <yutzu831126@gmail.com>
2022-10-22 14:56:12 +0800

    修正 1.url相對路徑 2.member_info.js為logout_ajax.js

--- src/main/webapp/js/member/logout_ajax.js
+++ src/main/webapp/js/member/logout_ajax.js
@@ -1,7 +1,7 @@
 //登出
 function logout(){
     $.ajax({
-        url: "http://localhost:8080/simplefitness-servlet/member/logout",
+        url: "/simplefitness-servlet/member/logout",
             type: "GET",
             // async: false,
             contentType: 'application/json; charset=UTF-8',
